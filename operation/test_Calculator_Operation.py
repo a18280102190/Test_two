7f0de43809a9314f1d873e74dd71afaf2366fa1e
a18280102190 <1340839041@qq.com>
2020-08-20 00:07:51 +0800

    ### web 企业微信实战（一）

--- operation/test_Calculator_Operation.py
+++ operation/test_Calculator_Operation.py
@@ -14,32 +14,36 @@ from Homework.calculator import Calculator
 
 
 def get_datas():
-    with open('./clacul.yaml',encoding='utf-8') as f:
+    with open('./clacul.yaml', encoding='utf-8') as f:
         mydatas = yaml.safe_load(f)
         adddatas = mydatas['add']['datas']
         myids = mydatas['add']['myids']
     return [adddatas, myids]
 
+
 def get_datas1():
-    with open('./clacul.yaml',encoding='utf-8') as f1:
+    with open('./clacul.yaml', encoding='utf-8') as f1:
         mydata1 = yaml.safe_load(f1)
         subdata1 = mydata1['sub']['datas1']
         myids1 = mydata1['sub']['myids1']
-    return [subdata1,myids1]
+    return [subdata1, myids1]
+
 
 def get_datas2():
-    with open('clacul.yaml',encoding='utf-8') as f2:
+    with open('clacul.yaml', encoding='utf-8') as f2:
         mydatas2 = yaml.safe_load(f2)
         muldata2 = mydatas2['mul']['datas2']
         myids2 = mydatas2['mul']['myids2']
-    return [muldata2,myids2]
+    return [muldata2, myids2]
+
 
 def get_datas3():
-    with open('./clacul.yaml',encoding='utf-8') as f3:
+    with open('./clacul.yaml', encoding='utf-8') as f3:
         mydatas3 = yaml.safe_load(f3)
         divdata3 = mydatas3['div']['datas3']
         myids3 = mydatas3['div']['myids3']
-    return [divdata3,myids3]
+    return [divdata3, myids3]
+
 
 class TestCalcu():
 
@@ -55,10 +59,9 @@ class TestCalcu():
                              get_datas()[0],
                              ids=get_datas()[1]
                              )
-
     def test_add(self, a, b, expect):
         print('计算相加')
-        results = round(self.calc.add(a,b),2)
+        results = round(self.calc.add(a, b), 2)
         assert expect == results
         print(results)
 
@@ -66,29 +69,28 @@ class TestCalcu():
                              get_datas1()[0],
                              ids=get_datas1()[1]
                              )
-    def test_sub(self,a,b,expect):
+    def test_sub(self, a, b, expect):
         print('计算相减')
-        results = self.calc.sub(a,b)
+        results = self.calc.sub(a, b)
         assert expect == results
         print(results)
-
 
     @pytest.mark.parametrize('a,b,expect',
                              get_datas2()[0],
                              ids=get_datas2()[1]
                              )
-    def test_mul(self,a,b,expect):
+    def test_mul(self, a, b, expect):
         print('计算相乘')
-        results = self.calc.mul(a,b)
-        assert  expect == results
+        results = self.calc.mul(a, b)
+        assert expect == results
         print(results)
 
     @pytest.mark.parametrize('a,b,expect',
                              get_datas3()[0],
                              ids=get_datas3()[1]
                              )
-    def test_div(self,a,b,expect):
+    def test_div(self, a, b, expect):
         print('计算相除')
-        results = self.calc.div(a,b)
+        results = self.calc.div(a, b)
         assert expect == results
         print(results)
