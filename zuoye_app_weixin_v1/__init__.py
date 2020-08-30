a5eab43d886b8d55d53905e30859aa750fc6639b
a18280102190 <1340839041@qq.com>
2020-08-30 18:21:36 +0800

    ### app企业微信实战（一)

--- zuoye_app_weixin_v1/__init__.py
+++ zuoye_app_weixin_v1/__init__.py
@@ -4,6 +4,6 @@
 @Project -> File   ：Test_two -> __init__.py
 @IDE    ：PyCharm
 @Author ：hang.zhao
-@Date   ：2020/8/30 16:41
+@Date   ：2020/8/30 18:20
 @Desc   ：
 =================================================='''
