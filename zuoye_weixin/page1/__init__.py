939400b1a569a38ed2daf97105f9cfb8ea4dbfde
a18280102190 <1340839041@qq.com>
2020-11-09 11:20:40 +0800

    delete zuoye_weixin

--- zuoye_weixin/page1/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-'''=================================================
-@Project -> File   ：Test_two -> __init__.py
-@IDE    ：PyCharm
-@Author ：hang.zhao
-@Date   ：2020/8/23 13:03
-@Desc   ：
-=================================================='''
